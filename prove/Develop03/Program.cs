624db2d00d2036edf298ad84d448dcc675846490
Dragron1996 <Dragron1996@gmail.com>
2024-03-16 23:48:10 -0600

    going extra

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,10 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
-        Scripture John = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        var John = new List<Scripture>
+        {
+            new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."),
+            new Scripture("1 Corinthians 10:31", "Whether therefore ye eat, or drink, or whatsoever ye do, do all to the glory of God."),
+            new Scripture("1 John 1:9", "If we confess our sins, he is faithful and just to forgive us our sins, and to cleanse us from all unrighteousness."),
 
+        };
+
+        Random vars = new Random();
+        var varsScripture = John[vars.Next(John.Count)];
         Console.Clear();
-        John.DisplayScripture();
+        varsScripture.DisplayScripture();
 
         while(true)
         {
@@ -22,10 +30,10 @@ class Program
             else
             {
                 Console.Clear();
-                John.HideWord();
-                John.DisplayScripture();
+                varsScripture.HideWord();
+                varsScripture.DisplayScripture();
 
-                if(John.AllWordsHidden())
+                if(varsScripture.AllWordsHidden())
                 {
                     Console.WriteLine("Finished!");
                     break;
